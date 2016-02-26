8ade3cad131db2fa09cb10e20e9a805db878dda1
Mark Li <markruoleili@gmail,com>
2016-02-26 12:14:39 -0800

    adding a movie_list view

--- starter/urls.py
+++ starter/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(__package__ + '.views',
     url(r'^$', 'landing_page', {}, 'landing_page'),
+    url(r'^movie_list/', views.movie_list, name='movie_list'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- starter/views.py
+++ starter/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from djang.http import HttpResponse
 
 def landing_page(request):
     return render(request, "starter/landing_page.html")
+
+def movie_list(request):
+	return HttpResponse('movie list page')
